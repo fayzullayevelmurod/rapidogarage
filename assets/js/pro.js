9fc34a97c85d422acb0469ba399b277dca9c9f2c
asadbekqulboyev <asadbekqulboyev@gmail.com>
2024-05-09 15:52:21 +0500

    advantages

--- assets/js/pro.js
+++ assets/js/pro.js
@@ -11,3 +11,22 @@ $('.menu_item').hover(
         $(this).find('.sub_menu').removeClass('active')
     }
 )
+
+// slide gallery
+var galleryCar = new Swiper(".galleryCar", {
+    slidesPerView: 5,
+    centeredSlides: true,
+    spaceBetween: 70,
+    loop:true,
+    grabCursor: true,
+    breakpoints:{
+        0:{
+            spaceBetween: 5,
+            slidesPerView: 4,
+        },
+        1024:{
+            slidesPerView: 5,
+            spaceBetween: 70,
+        }
+    }
+  });
